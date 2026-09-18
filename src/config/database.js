f7422dbc97c84ca97f3eaf9ac4a65e99520de4b2
juliasfreire <juliasf590@gmail.com>
2026-09-18 17:14:08 -0300

    Alteração no database

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,15 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     waitForConnections:true,
     connectionLimit: 10,
     queueLimit: 0
