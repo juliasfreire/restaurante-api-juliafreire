25eb6710a30cdc420c90e0adac221702adf19356
juliasfreire <juliasf590@gmail.com>
2026-09-25 17:19:23 -0300

    Alterando cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const PORT = 3001
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "https://restaurante-front-juliafreire-mbed-b88o4krzm-julia-freire.vercel.app",
+    credentials: true
+}));
 
 app.get("/",(req,res)=>{
     res.json({
